670cc8434ec02a96ecf70d974bc95862b9982649
Xen0mrefne <x3n0g3n1c@gmail.com>
2023-09-08 15:50:13 -0300

    wave animation finished

--- wave/js/script.js
+++ wave/js/script.js
@@ -1,10 +1,12 @@
 const canvas = document.getElementById("waves-canvas")
 
+
 const wave = {
     x: -Math.PI * 8,
     y: window.innerHeight - 100,
-    height: 15,
-    frequency: 10
+    height: 30,
+    frequency: 10,
+    displacement: 0
 }
 
 function draw() {
@@ -13,42 +15,46 @@ function draw() {
 
     ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
 
-    let x = wave.x
+    let x;
+    x = wave.x
     ctx.beginPath()
     ctx.moveTo(x, wave.y)
     do {
-        ctx.fillStyle = "#2af"
-        
-        ctx.lineTo(x, wave.y + (wave.height * Math.sin(x / 16)))
-        x += Math.PI * 4
-        
-    } while (x < window.innerWidth + (Math.PI * 8) * 2)
+        ctx.fillStyle = "#1af"
+       
+        ctx.lineTo(x, wave.y + (wave.height * Math.sin(x / 64 - wave.displacement)))
+        x += Math.PI * 8
+    } while (x < window.innerWidth + (Math.PI * 8))
 
     ctx.lineTo(x, window.innerHeight)
     ctx.lineTo(0, window.innerHeight)
-
-    x = -Math.PI * 8
-    ctx.lineTo(wave.x, wave.y)
-    
+    ctx.lineTo(0, wave.y)
     ctx.fill()
+    wave.displacement += 0.1
 
-    wave.x -= Math.PI / 8
-
+    if (wave.displacement > Math.PI * 2) {
+        wave.displacement = 0
+    }
 
     window.requestAnimationFrame(draw)
 }
 
+
 function init() {
+
 
     canvas.setAttribute("width", window.innerWidth)
     canvas.setAttribute("height", window.innerHeight)
+
 
     window.addEventListener("resize", () => {
         canvas.setAttribute("width", window.innerWidth)
         canvas.setAttribute("height", window.innerHeight)
     })
 
+
     window.requestAnimationFrame(draw)
 }
+
 
 window.addEventListener("load", init)
